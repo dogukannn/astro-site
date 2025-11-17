6b9e882f962bdce97b1d1c44118368ffa57c3b46
dogukannn <dogukan.ozturk9@outlook.com>
2025-11-17 22:35:21 +0300

    update site

--- astro.config.ts
+++ astro.config.ts
@@ -19,7 +19,8 @@ import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  site: 'https://astro-erudite.vercel.app',
+  site: 'https://dogukannn.github.io/astro-site',
+  base: '/astro-site',
   integrations: [
     expressiveCode({
       themes: ['github-light', 'github-dark'],
